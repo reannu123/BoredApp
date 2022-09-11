54fbf15ef307653fe56ac480cf7b7485e0486231
reannu123 <reannumon123@gmail.com>
2022-09-11 16:34:28 +0000

    Set up simple login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,41 @@
+import { Alert, Container, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <>
+      <Container className="py-5 my-5 border rounded-3 d-flex align-items-center justify-content-center">
+        <Form className="border">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+}
+
+export default Login;
