4d0f62b4be2270dd3146abc94bd56ff25f90341f
reannu123 <reannumon123@gmail.com>
2023-03-15 21:31:39 +0800

    Separated server components of the app

--- server/controllers/MainController.js
+++
@@ -1,7 +0,0 @@
-class MainController {
-  static async index(req, res, next) {
-    res.send(["user", "task"]);
-  }
-}
-
-module.exports = MainController;
--- server/dao/TokensDAO.js
+++
@@ -1,8 +0,0 @@
-const TokenModel = require("../models/TokenModel");
-
-class TokenDAO {
-  static async findToken() {}
-  static async deleteToken() {}
-}
-
-module.exports = TokenDAO;
