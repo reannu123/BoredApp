d68df606856997d35dac82edff026fff8db704d0
reannu123 <reannumon123@gmail.com>
2023-06-08 23:34:11 +0800

    Updated to new server address

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://reannuinstrella.tech/",
+  baseURL: "https://api.reannuinstrella.site/",
 });
 
 const auth = axios.create({
-  baseURL: "https://reannuinstrella.tech/auth",
+  baseURL: "https://auth.reannuinstrella.site/auth",
 });
 
 export { instance, auth };
